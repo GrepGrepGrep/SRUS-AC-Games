20161b0f3ad513df5b5ebff6ebc6c532d47e4be9
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-02-16 14:12:37 +0800

    fixed recoursion error, made pretty print

--- app/player_node.py
+++ app/player_node.py
@@ -6,8 +6,17 @@ class PlayerNode:
         self.next = None
         self.previous = None
         self.player = player
-        self.key = self.player._uid
 
-    def __str__(self):
-        return "NEXT: {} | PREVIOUS: {} | KEY: {}".format(self.next, self.previous, self.key)
 
+    def display(self):
+        next_p = self.next
+        previous = self.previous
+        if next_p is not None:
+            next_p = next_p.player._name;
+
+        if previous is not None:
+            previous = previous.player._name;
+        print("------------------------------------")
+        print("previous | current | next")
+        print("{} <- {} -> {}".format( previous, self.player._name,  next_p,))
+        print("------------------------------------")
