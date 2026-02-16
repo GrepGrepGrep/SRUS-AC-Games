06d16a577f39293f9b140679312f67a1a20bd78d
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-02-16 12:59:07 +0800

    whitespace + fixed instion + added print

--- app/player_list.py
+++ app/player_list.py
@@ -1,13 +1,8 @@
-
-
 from app.player import Player
 from app.player_node import PlayerNode
 
 
 class PlayerList:
-
-
-
 
     def __init__(self):
         self.first = None
@@ -17,16 +12,15 @@ class PlayerList:
         return self.first is None
 
     def insert_first(self, new_player=Player):
-        player_node =  PlayerNode(new_player)
+        player_node = PlayerNode(new_player)
 
         if self.is_empty():
             self.first = player_node
             self.last = player_node
             return
 
-
-        self.first._previous = player_node
         player_node.next = self.first
+        self.first.previous = player_node
         self.first = player_node
 
     def insert_last(self, new_player=Player):
@@ -37,15 +31,17 @@ class PlayerList:
             self.last = player_node
             return
 
-        player_node._previous = self.last
         self.last.next = player_node
+        player_node.previous = self.last
         self.last = player_node
 
+    def __str__(self):
+        buf = "items: ["
+        current = self.first
 
-
-
-
-
-
-
-
+        while current is not None:
+            buf += " " + str(current.player.name)
+            # buf += str(current)
+            current = current.next
+        buf += " ]"
+        return buf
