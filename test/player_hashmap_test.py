83f5fa25078d36e22c644db7d27bfb37a2bfa86d
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-03-23 11:43:58 +0800

    chore added tests

---
+++ test/player_hashmap_test.py
@@ -0,0 +1,44 @@
+# https://docs.python.org/3/library/unittest.html
+
+import unittest
+
+from app.player import Player
+from app.player_hashmap import PlayerHashMap
+
+
+def data():
+    p = PlayerHashMap(10)
+    p.put(Player(_uid=11, _name="bob"))
+    p.put(Player(_uid=1, _name="ava"))
+    p.put(Player(_uid=20, _name="dave"))
+    p.put(Player(_uid=10, _name="steve"))
+    return p
+
+
+class TestPlayerMethods(unittest.TestCase):
+
+    def test_uid(self):
+        p = data()
+        p.put(Player(_uid=11, _name="bob_again"))
+        # length should be 4
+        self.assertEqual(p.len(), 4)
+
+    def test_get_none(self):
+        p = data()
+        self.assertIsNone(p.get(124))
+
+    def test_get(self):
+        p = data()
+        self.assertIsNotNone(p.get(11))
+
+    def test_remove(self):
+        p = data()
+        self.assertIsNotNone(p.get(11))
+        p.remove(11)
+        self.assertIsNone(p.get(11))
+
+    def test_len(self):
+        p = data()
+        self.assertEqual(p.len(), 4)
+        p.remove(11)
+        self.assertEqual(p.len(), 3)
