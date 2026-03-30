45ca6b691b6e65ab735fd6cc303b4c0404c576f6
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-03-30 14:05:23 +0800

    feat: added score

--- app/player.py
+++ app/player.py
@@ -1,8 +1,9 @@
 class Player:
 
-    def __init__(self, _uid, _name):
+    def __init__(self, _uid, _name, _score):
         self._uid = _uid
         self._name = _name
+        self._score = _score
 
 
     def __str__(self):
