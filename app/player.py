b1827ddba00ead5744d996398be660eec4b9773d
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-03-30 14:09:17 +0800

    chore: added default value

--- app/player.py
+++ app/player.py
@@ -1,6 +1,7 @@
 class Player:
 
-    def __init__(self, _uid: int, _name: str, _score: int):
+    def __init__(self, _uid: int, _name: str, _score: int = 0) -> None:
+
         self._uid = _uid
         self._name = _name
         self._score = _score
