553667a2aea6e24a747b8e521ffbea1bdd3c0866
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-03-30 14:06:00 +0800

    chore: added type hints

--- app/player.py
+++ app/player.py
@@ -1,6 +1,6 @@
 class Player:
 
-    def __init__(self, _uid, _name, _score):
+    def __init__(self, _uid: int, _name: str, _score: int):
         self._uid = _uid
         self._name = _name
         self._score = _score
