db77dbc7f819192dab188d3f3b4a8f818473adfc
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-03-30 14:26:37 +0800

    chore: added setter for score

--- app/player.py
+++ app/player.py
@@ -1,11 +1,31 @@
+# https://machinelearningplus.com/python/python-property-getters-setters-and-attribute-control-guide/
+
+def score_validate(score):
+    if score < 0:
+        raise ValueError('Invalid value, positive scores only')
+
+
 class Player:
 
     def __init__(self, _uid: int, _name: str, _score: int = 0) -> None:
-
+        score_validate(_score)
         self._uid = _uid
         self._name = _name
         self._score = _score
 
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score: int = 0):
+        score_validate(score)
+        self._score = score
 
     def __str__(self):
-        return "UID: {} | name: {}" .format(self._uid, self._name)
+        return "UID: {} | name: {}".format(self._uid, self._name)
+
+
+
+player = Player(3, "", _score=2)
+player._score = -2
