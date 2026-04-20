4a473c26a62322d68d7dbcf681faf1cdcb5dcb39
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-04-20 12:55:02 +0800

    fix: implemented property for value

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,4 +1,15 @@
+from app.player import Player
+
+
 class PlayerBST:
 
     def __init__(self):
-        self.value = None
+        self.__value = None
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value: Player):
+        self.__value = value
