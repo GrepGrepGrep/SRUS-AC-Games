effaf66082212fd05efa469051406b36c666768c
ToastXC <100072983+toastxc@users.noreply.github.com>
2026-04-23 16:38:10 +0800

    feat: added BST search

--- app/player_bst.py
+++ app/player_bst.py
@@ -27,16 +27,44 @@ class PlayerBST:
         if player._name < node.root._name:
             if node.left is None:
                 node.left = PlayerBNode(player)
-                return
             else:
                 self.__insert_inner(player, node.left)
         else:
             if node.right is None:
                 node.right = PlayerBNode(player)
-                return
+
             else:
                 self.__insert_inner(player, node.right)
 
+    def __search_inner(self, name: str, node: PlayerBNode) -> bool:
+        if name == node.root._name:
+            return True
+
+        if name < node.root._name:
+            if node.left is None:
+                # node.left = PlayerBNode(player)
+                return False
+            else:
+                self.__search_inner(name, node.left)
+        else:
+            if node.right is None:
+                # node.right = PlayerBNode(player)
+                return False
+
+            else:
+                self.__search_inner(name, node.right)
+
+    def search(self, name: str)-> bool:
+        return self.__search_inner(name, self.value)
 
 
 
+
+b = PlayerBST()
+b.value = Player("0", "3", 5)
+b.insert(Player("0", "2", 5))
+b.insert(Player("0", "1", 1))
+b.insert(Player("0", "4", 4))
+b.insert(Player("0", "5", 1))
+
+b.search("5")
